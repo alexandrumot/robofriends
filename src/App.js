cc8ca59917c450746468164be3cbf513215c39a6
Hackerutz <Hackerutz.21@gmail.com>
2021-08-04 15:20:40 +0300

    changed message to display

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload browser.
           </p>
           <a
           className="App-link"
